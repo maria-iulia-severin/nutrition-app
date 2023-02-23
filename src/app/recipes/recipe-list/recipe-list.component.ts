74ff9f38783a4561715ffb4b270cfd1976c599c7
Maria Iulia Severin <55393346+maria-iulia-severin@users.noreply.github.com>
2023-02-23 20:55:11 +0200

    Recipe model added and used

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Description is here', 'https://www.seriouseats.com/thmb/IKmZuaIJ_ZEIurITc-UznICtgFg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20210712-tacos-al-pastor-melissa-hom-seriouseats-37-f72cdd02c9574bceb1eef1c8a23b76ed.jpg'),
+    new Recipe('Second Recipe', 'Description is here', 'https://www.seriouseats.com/thmb/IKmZuaIJ_ZEIurITc-UznICtgFg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20210712-tacos-al-pastor-melissa-hom-seriouseats-37-f72cdd02c9574bceb1eef1c8a23b76ed.jpg')
 
+  ]; //array with type Recipe - using the model Recipe
 }
